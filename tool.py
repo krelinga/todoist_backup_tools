8e7aaf844828091199c4be44cc6ef82237f274f4
Andy Kreling <krelinga@gmail.com>
2023-09-08 15:02:59 -0500

    parse tasks & notes into different types and merge them into a hierarchy.

--- tool.py
+++ tool.py
@@ -20,6 +20,48 @@ class CSVLine:
     timezone: str
 
 
+@dataclasses.dataclass
+class TodoistNote:
+    content: str
+    author: str
+    date: str
+
+
+@dataclasses.dataclass
+class TodoistTask:
+    content: str
+    description: str
+    priority: str
+    indent: str
+    author: str
+    responsible: str
+    date: str
+    date_lang: str
+    timezone: str
+    notes: list[TodoistNote] = dataclasses.field(default_factory=list)
+
+
+def ConvertCsvLinesToTasks(csv_lines: list[CSVLine]) -> list[TodoistTask]:
+    def copy_relevant_fields(csv_line: CSVLine, out_type):
+        csv_line_as_dict = dataclasses.asdict(csv_line)
+        copied_values = {}
+        for field_name in map(lambda x: x.name, dataclasses.fields(out_type)):
+            if field_name in csv_line_as_dict:
+                copied_values[field_name] = csv_line_as_dict[field_name]
+        return out_type(**copied_values)
+
+    todoist_tasks = []
+    for csv_line in csv_lines:
+        if csv_line.type == 'task':
+            todoist_tasks.append(copy_relevant_fields(csv_line, TodoistTask))
+        elif csv_line.type == 'note':
+            assert len(todoist_tasks) > 0
+            todoist_tasks[-1].notes.append(copy_relevant_fields(csv_line, TodoistNote))
+        else:
+            assert False, f'unrecognized type {csv_line.type}'
+    return todoist_tasks
+
+
 def ReadFile(csv_path: str) -> list[CSVLine]:
     csv_lines = []
     with open(csv_path, newline='', encoding='utf-8-sig') as csv_file:
@@ -34,8 +76,9 @@ def main():
     assert len(sys.argv) == 2, sys.argv
     csv_path = sys.argv[1]
     entries = filter(lambda x: x.type != '', ReadFile(csv_path))
-    for entry in entries:
-        print(entry)
+    tasks = ConvertCsvLinesToTasks(entries)
+    for task in tasks:
+        print(task)
 
 
 if __name__ == '__main__':
