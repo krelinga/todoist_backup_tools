6f51fb25eb33b0090484e0fdba7150022c386763
Andy Kreling <krelinga@gmail.com>
2023-09-08 12:40:26 -0500

    command line flag to specify which file to process.

--- tool.py
+++ tool.py
@@ -1,8 +1,15 @@
 #! /usr/bin/python3
 
 
+import csv
+import sys
+
+
 def main():
     print('hello world!')
+    assert len(sys.argv) == 2, sys.argv
+    csv_path = sys.argv[1]
+    print(csv_path)
 
 
 if __name__ == '__main__':
