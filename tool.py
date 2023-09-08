8ef5cd23d7ffa4f3db969477a742291a5a220771
Andy Kreling <krelinga@gmail.com>
2023-09-08 12:48:17 -0500

    use DictReader instead.

--- tool.py
+++ tool.py
@@ -9,9 +9,9 @@ def main():
     assert len(sys.argv) == 2, sys.argv
     csv_path = sys.argv[1]
     with open(csv_path, newline='') as csv_file:
-        csv_reader = csv.reader(csv_file)
+        csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            print('\t'.join(row))
+            print(row)
 
 
 if __name__ == '__main__':
