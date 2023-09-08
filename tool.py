c051bf30a949a7079ad4b5d52b857230cc8a8746
Andy Kreling <krelinga@gmail.com>
2023-09-08 12:49:21 -0500

    specify an encoding, this fixes some weird unicode characters that otherwise show up.

--- tool.py
+++ tool.py
@@ -8,7 +8,7 @@ import sys
 def main():
     assert len(sys.argv) == 2, sys.argv
     csv_path = sys.argv[1]
-    with open(csv_path, newline='') as csv_file:
+    with open(csv_path, newline='', encoding='utf-8-sig') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             print(row)
